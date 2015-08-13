1172da29fac48eee67a4f9c64293b2844828801d
Chris Birchall <chris.birchall@guardian.co.uk>
2015-08-13 17:41:29 +0100

    Fix lack of mascot

--- app/AppLoader.scala
+++ app/AppLoader.scala
@@ -2,5 +2,34 @@ import play.api.{ Application, ApplicationLoader }
 import play.api.ApplicationLoader.Context
 
 class AppLoader extends ApplicationLoader {
-  override def load(context: Context): Application = new AppComponents(context).application
+  override def load(context: Context): Application = {
+    val components = new AppComponents(context)
+
+    println("""
+                          /\          /\
+                         ( \\        // )
+                          \ \\      // /
+                           \_\\||||//_/
+                            \/ _  _ \
+                           \/|(O)(O)|
+                          \/ |      |
+      ___________________\/  \      /
+     //                //     |____|
+    //                ||     /      \
+   //|                \|     \ 0  0 /
+  // \       )         V    / \____/
+ //   \     /        (     /
+""     \   /_________|  |_/
+       /  /\   /     |  ||
+      /  / /  /      \  ||
+      | |  | |        | ||
+      | |  | |        | ||
+      |_|  |_|        |_||
+       \_\  \_\        \_\\ 
+
+  The Dashboard Donkey welcomes you.
+""")
+
+    components.application
+  }
 }
